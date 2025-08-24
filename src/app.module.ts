e793ef1e4e3d6653dddb898966077dcc126b66df
wssimplicio <wsalessimplicio@gmail.com>
2025-08-24 17:23:44 -0300

    Config conection for postgres and migration with typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+     TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number(process.env.DB_PORT) || 8745,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'supertux',
+      database: process.env.DB_DATABASE || 'exemplo',
+      synchronize: false,
+      migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+      migrationsRun: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/migrations/1756066604226-CreateTableUser.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateTableUser1756066604226 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE "user" (
+                "id" SERIAL PRIMARY KEY,
+                "name" VARCHAR(100) NOT NULL,
+                "email" VARCHAR(100) NOT NULL UNIQUE,
+                "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE "user"
+        `);
+    }
+
+}
